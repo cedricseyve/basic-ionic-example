6476193d0c7d433a0a2c1286d310b6ec738a1a41
cseyve <seyve.1508@apside-groupe.com>
2022-04-06 09:30:07 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lg-ionic-ex',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
