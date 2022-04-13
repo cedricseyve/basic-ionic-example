893ff221f666e731e635df652645b0f19e627276
cseyve <seyve.1508@apside-groupe.com>
2022-04-13 15:47:55 +0200

    implemented a basic calendar for ionic

--- src/app/pages/tabs/tab2/tab2.page.ts
+++ src/app/pages/tabs/tab2/tab2.page.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CalendarMode, Step } from 'ionic2-calendar/calendar';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  eventSource;
+  viewTitle;
+
+  isToday: boolean;
+  calendar = {
+    mode: 'month' as CalendarMode,
+    step: 30 as Step,
+    // Required :
+    currentDate: new Date(),
+    dateFormatter: {
+      formatMonthViewDay: function (date: Date) {
+        return date.getDate().toString();
+      },
+      formatMonthViewDayHeader: function (date: Date) {
+        return 'MonMH';
+      },
+      formatMonthViewTitle: function (date: Date) {
+        return 'testMT';
+      },
+      formatWeekViewDayHeader: function (date: Date) {
+        return 'MonWH';
+      },
+      formatWeekViewTitle: function (date: Date) {
+        return 'testWT';
+      },
+      formatWeekViewHourColumn: function (date: Date) {
+        return 'testWH';
+      },
+      formatDayViewHourColumn: function (date: Date) {
+        return 'testDH';
+      },
+      formatDayViewTitle: function (date: Date) {
+        return 'testDT';
+      },
+    },
+  };
 
   constructor() {}
 
+  ngOnInit(): void {}
+
+  onViewTitleChanged(title) {
+    this.viewTitle = title;
+  }
+
+  onEventSelected(event) {
+    console.log('Event selected:' + event.startTime + '-' + event.endTime + ',' + event.title);
+  }
+
+  onTimeSelected(ev) {
+    console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' + (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
+  }
+
+  onCurrentDateChanged(event: Date) {
+    var today = new Date();
+    today.setHours(0, 0, 0, 0);
+    event.setHours(0, 0, 0, 0);
+    this.isToday = today.getTime() === event.getTime();
+  }
 }
--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,13 +1,14 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {IonicModule} from '@ionic/angular';
+import {NgCalendarModule} from 'ionic2-calendar';
 
 import {LgCardComponent} from './components/lg-card/lg-card.component';
 import {CheckAccessDirective} from './directives/check-access.directive';
 
 // Shared components and modules must be imported AND exported
 const sharedComponents = [LgCardComponent, CheckAccessDirective];
-const sharedModules = [];
+const sharedModules = [NgCalendarModule];
 
 @NgModule({
   declarations: sharedComponents,
